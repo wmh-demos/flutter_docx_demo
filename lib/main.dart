075aa35692371acdbee343d66d0d4fad189e9c6f
weimuhua <weimuhua0812@126.com>
2022-12-18 21:43:11 +0800

    feat: 精简docx和写入内容

--- lib/main.dart
+++ lib/main.dart
@@ -112,79 +112,8 @@ class _MyHomePageState extends State<MyHomePage> {
     final bytes = data.buffer.asUint8List();
     final docx = await DocxTemplate.fromBytes(bytes);
 
-    final listNormal = ['Foo', 'Bar', 'Baz'];
-    final listBold = ['ooF', 'raB', 'zaB'];
-
-    final contentList = <Content>[];
-
-    final b = listBold.iterator;
-    for (var n in listNormal) {
-      b.moveNext();
-
-      final c = PlainContent("value")
-        ..add(TextContent("normal", n))
-        ..add(TextContent("bold", b.current));
-      contentList.add(c);
-    }
-
     Content c = Content();
     c
-      ..add(TextContent("docname", "This is My House!!!"))
-      ..add(TextContent("passport", "Passport NE0323 4456673"))
-      ..add(TableContent("table", [
-        RowContent()
-          ..add(TextContent("key1", "Paul"))
-          ..add(TextContent("key2", "Viberg"))
-          ..add(TextContent("key3", "Engineer")),
-        RowContent()
-          ..add(TextContent("key1", "Alex"))
-          ..add(TextContent("key2", "Houser"))
-          ..add(TextContent("key3", "CEO & Founder"))
-          ..add(ListContent("tablelist", [
-            TextContent("value", "Mercedes-Benz C-Class S205"),
-            TextContent("value", "Lexus LX 570")
-          ]))
-      ]))
-      ..add(ListContent("list", [
-        TextContent("value", "Engine")
-          ..add(ListContent("listnested", contentList)),
-        TextContent("value", "Gearbox"),
-        TextContent("value", "Chassis")
-      ]))
-      ..add(ListContent("plainlist", [
-        PlainContent("plainview")
-          ..add(TableContent("table", [
-            RowContent()
-              ..add(TextContent("key1", "Paul"))
-              ..add(TextContent("key2", "Viberg"))
-              ..add(TextContent("key3", "Engineer")),
-            RowContent()
-              ..add(TextContent("key1", "Alex"))
-              ..add(TextContent("key2", "Houser"))
-              ..add(TextContent("key3", "CEO & Founder"))
-              ..add(ListContent("tablelist", [
-                TextContent("value", "Mercedes-Benz C-Class S205"),
-                TextContent("value", "Lexus LX 570")
-              ]))
-          ])),
-        PlainContent("plainview")
-          ..add(TableContent("table", [
-            RowContent()
-              ..add(TextContent("key1", "Nathan"))
-              ..add(TextContent("key2", "Anceaux"))
-              ..add(TextContent("key3", "Music artist"))
-              ..add(ListContent(
-                  "tablelist", [TextContent("value", "Peugeot 508")])),
-            RowContent()
-              ..add(TextContent("key1", "Louis"))
-              ..add(TextContent("key2", "Houplain"))
-              ..add(TextContent("key3", "Music artist"))
-              ..add(ListContent("tablelist", [
-                TextContent("value", "Range Rover Velar"),
-                TextContent("value", "Lada Vesta SW Sport")
-              ]))
-          ])),
-      ]))
       ..add(ListContent("multilineList", [
         PlainContent("multilinePlain")
           ..add(TextContent('multilineText', 'line 1')),
